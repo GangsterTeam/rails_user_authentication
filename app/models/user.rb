91538bbdcc7f2ae3474d59587a64daa89d32c988
BK <baladkb@gmail.com>
2019-05-29 18:04:34 +0530

    updates in index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord
+ class User < ApplicationRecord
   has_many :blogs
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
